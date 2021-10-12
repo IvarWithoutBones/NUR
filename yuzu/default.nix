0bdbbf074d3dbb6b55e9e0f6b2b63341a5bec50d
gh-actions <IvarWithoutBones@localhost>
2021-10-12 13:46:17 +0000

    Update packages to their latest version

--- yuzu/default.nix
+++ yuzu/default.nix
@@ -4,13 +4,13 @@ let
 in {
   mainline = libsForQt5.callPackage ./base.nix rec {
     pname = "yuzu-mainline";
-    version = "776";
+    version = "777";
     branchName = branch;
     src = fetchFromGitHub {
       owner = "yuzu-emu";
       repo = "yuzu-mainline";
       rev = "mainline-0-${version}";
-      sha256 = "1y1q18dc7lkgjmnvnsc63psychy7q9zsjc91ga8m76rpq1l587fc";
+      sha256 = "116b5v49jdqybnxb93lm0j26dwh5fg38cw382zq0j3541yr8ncs9";
       fetchSubmodules = true;
     };
   };
