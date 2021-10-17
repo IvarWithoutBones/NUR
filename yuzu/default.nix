a4a7976ec52f7d48d6bb32f02f9ddc54a2498358
gh-actions <IvarWithoutBones@localhost>
2021-10-17 13:58:56 +0000

    Update packages to their latest version

--- yuzu/default.nix
+++ yuzu/default.nix
@@ -4,25 +4,25 @@ let
 in {
   mainline = libsForQt5.callPackage ./base.nix rec {
     pname = "yuzu-mainline";
-    version = "781";
+    version = "782";
     branchName = branch;
     src = fetchFromGitHub {
       owner = "yuzu-emu";
       repo = "yuzu-mainline";
       rev = "mainline-0-${version}";
-      sha256 = "0202qrlkz91rgmj0i8kpmcdimp5pvpmxcbkfn48z1w8562hkyzfq";
+      sha256 = "1qnm8hxxcrg187r69sf5dnql2xjgnhs61nmqgpr1nxh4lyxwsbvz";
       fetchSubmodules = true;
     };
   };
   early-access = libsForQt5.callPackage ./base.nix rec {
     pname = "yuzu-ea";
-    version = "2146";
+    version = "2147";
     branchName = branch;
     src = fetchFromGitHub {
       owner = "pineappleEA";
       repo = "pineapple-src";
       rev = "EA-${version}";
-      sha256 = "1n8hy5lrnz41cy16wp7sb5k2czzq31rii5pwd7i6k4566vhc0szf";
+      sha256 = "1ncbci125fsinlgcgchml8c3g37r935pzwb06bly47jycvxxr2w5";
       fetchSubmodules = true;
     };
   };
